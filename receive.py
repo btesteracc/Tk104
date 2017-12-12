d78bb335630a06baa998d2f5fad5415bf0d81592
btesteracc <bjoern.proepper@gmail.com>
2017-12-12 20:09:50 +0100

    Überträgt jetzt das Datum als Epoch und nicht mehr als isoformat.

--- receive.py
+++ receive.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import os
 import sys
 import json
 import datetime
-import paho.mqtt.client as mqtt
 import re
 import logging
 import ssl
+import paho.mqtt.client as mqtt
 from python_utils import converters
 
 import receiver_config as conf
@@ -18,23 +17,23 @@ logging.basicConfig(level=logging.INFO)
 
 
 def get_date_from_line(dateline):
-    if dateline!=0:
-        format="T:%y/%m/%d %H:%M\n"
+    if dateline != "":
+        format_str = "T:%y/%m/%d %H:%M\n"
         try:
-            date_obj=datetime.datetime.strptime(content[dateline],format)
+            #return datetime.datetime.strptime(dateline, format_str).isoformat()
+            return datetime.datetime.strptime(dateline, format_str).timestamp()
         except ValueError:
-            date_obj=datetime.datetime.now()
-        return date_obj.strftime(format)
-    
+            #return datetime.datetime.now().isoformat()
+            return datetime.datetime.now().timestamp()
 
 #InFilename="/var/spool/gammu/inbox/"+sys.argv[1]
 def get_data():
-    InFilename=sys.argv[1]
-    with open(InFilename,'r') as InFile:
-        content=InFile.readlines()
-    InFile.close()
-    devicename=sys.argv[1].split("_")[3]
-    conf.topic+=devicename[1:]
+    in_filename = sys.argv[1]
+    with open(in_filename, 'r') as in_file:
+        content = in_file.readlines()
+    in_file.close()
+    devicename = sys.argv[1].split("_")[3]
+    conf.topic += devicename[1:]
     return content
 
 def check_case_and_format_to_json(content):
@@ -53,53 +52,51 @@ def check_case_and_format_to_json(content):
         data["lat"] = converters.to_float(content[1], regexp=True)
         data["lon"] = converters.to_float(content[2], regexp=True)
         data["batt"] = converters.to_int(content[-1], regexp=True)
-        data["desc"]=content[6]+content[3]
-        data["tst"]=get_date_from_line(7)
+        data["desc"] = content[6]+content[3]
+        data["tst"] = get_date_from_line(content[7])
         #checked i.O.
 
-    elif (content[0].startswith("lat")) and (content[0].find("lon")!=-1):
-        grids=re.findall("([-+]?\d{1,2}[.]\d+)",content[0])
-        data["lat"]=float(grids[0])
-        data["lon"]=float(grids[1])
+    elif (content[0].startswith("lat")) and (content[0].find("lon") != -1):
+        grids = re.findall("([-+]?\d{1,2}[.]\d+)", content[0])
+        data["lat"] = float(grids[0])
+        data["lon"] = float(grids[1])
         data["batt"] = converters.to_int(content[3], regexp=True)
-        data["tst"] = get_date_from_line(2)
+        data["tst"] = get_date_from_line(content[2])
         #checked i.O.
 
     elif content[0].startswith("lat") and content[1].startswith("long"):
         data["lat"] = converters.to_float(content[0], regexp=True)
-        data["lon"]= converters.to_float(content[1],regexp=True)
-        data["batt"] = converters.to_int(content[4],regexp=True)
-        data["tst"] = get_date_from_line(3)
+        data["lon"] = converters.to_float(content[1], regexp=True)
+        data["batt"] = converters.to_int(content[4], regexp=True)
+        data["tst"] = get_date_from_line(content[3])
         #checked i.O.
 
     elif content[0].startswith("Lac"):
-        grids=re.findall("([-+]?\d{1,2}[.]\d+)\,([-+]?\d{1,2}[.]\d+)",content[4])
-        data["lat"]=float(grids[0][0])
-        data["lon"]=float(grids[0][1])
-        data["desc"]=content[0]+content[4]
-        data["tst"] = get_date_from_line(1)
+        grids = re.findall("([-+]?\d{1,2}[.]\d+)\,([-+]?\d{1,2}[.]\d+)", content[4])
+        data["lat"] = float(grids[0][0])
+        data["lon"] = float(grids[0][1])
+        data["desc"] = content[0]+content[4]
+        data["tst"] = get_date_from_line(content[1])
         #checked i.O.
     return data
 
 
 def send_data(json_data):
-    if (json_data['tst']!=0) and (json_data['lat']!=0.0) and (json_data['lon']!=0.0):
-        datastr=json.dumps(json_data)
+    if (json_data['tst'] != 0) and (json_data['lat'] != 0.0) and (json_data['lon'] != 0.0):
+        datastr = json.dumps(json_data)
         logging.info(conf.topic)
         logging.info(datastr)
         mqttc = mqtt.Client()
         context = ssl.create_default_context()
         mqttc.tls_set_context(context)
-        mqttc.connect(conf.hostname,port=conf.port)
-
-        mqttc.publish(conf.topic,payload=datastr)
-        mqttc.disconnect
+        mqttc.connect(conf.hostname, port=conf.port)
+        mqttc.publish(conf.topic, payload=datastr)
+        mqttc.disconnect()
         logging.info("Datensatz gesendet")
     else:
         logging.info("Fehler im Format")
 
 
-content = get_data()
-json_data = check_case_and_format_to_json(content)
-send_data(json_data)
-
+CONTENT = get_data()
+JSON_DATA = check_case_and_format_to_json(CONTENT)
+send_data(JSON_DATA)
